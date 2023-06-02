a76e663eb53d96c959bba4485fa7b33adfb38593
Innovanito <khg05311@naver.com>
2023-06-02 18:50:51 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Routes} from "react-router-dom"
+
+function App() {
+
+  return (
+    // <Routes>
+
+    // </Routes>
+    <h1>hi</h1>
+  )
+}
+
+export default App
