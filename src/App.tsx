58a1551aaabd7d690ff2fc70a4f8fe7460f197f5
Innovanito <khg05311@naver.com>
2023-06-03 21:06:39 +0900

    meanwhile making Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Routes} from "react-router-dom"
+import { Container } from "react-bootstrap"
+import {Routes, Route, Navigate} from "react-router-dom"
+import NewNote from "./NewNote"
 
 function App() {
 
   return (
-    // <Routes>
-
-    // </Routes>
-    <h1>hi</h1>
+    <Container className="my-4">
+      <Routes>
+        <Route path='/' element={<h1>heeeeey</h1>}/>
+        <Route path='/new' element={<NewNote/>}/>
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+        </Route>
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </Container>
   )
 }
 
---
+++ src/NewNote.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewForm from './NewForm'
+
+const NewNote = () => {
+  return (
+    <>
+      <div className='mb-4'>NewNote</div>
+      <NewForm/>
+    </>
+  )
+}
+
+export default NewNote
